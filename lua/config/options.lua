9dd1606505897b5a0e6c19128bdfec9a5b54ba23
Andreas Guettinger <aguettinger@gmail.com>
2023-06-02 18:36:47 +0200

    fix luasnip

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,11 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing TAB will insert spaces
-vim.o.softtabstop = 4 -- Number of spaces inserted when pressing TAB
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-vim.opt.smartindent = true
+-- vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+-- vim.o.expandtab = true -- Pressing TAB will insert spaces
+-- vim.o.softtabstop = 4 -- Number of spaces inserted when pressing TAB
+-- vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+-- vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -7,6 +7,18 @@ return {
     },
   },
   {
+    "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
+  },
+
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  -- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
+  { import = "lazyvim.plugins.extras.lang.json" },
+
+  {
     -- Treesitter
     "nvim-treesitter/nvim-treesitter",
     opts = {
