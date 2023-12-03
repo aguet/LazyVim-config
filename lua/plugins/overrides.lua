9bd7c32b795bfc1ee6787bd2ef63841f2cb9a41f
Andreas Guettinger <aguettinger@gmail.com>
2023-12-03 15:51:20 +0100

    new version

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -6,8 +6,12 @@ return {
       { "<leader>pv", "<cmd>Neotree focus<cr>", desc = "Focus Neotree" },
     },
   },
+
   {
     "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
     build = "make install_jsregexp",
   },
 
